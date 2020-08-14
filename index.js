f7067f96377f5811cdd90c382f16ac706d17b101
Danny Currie <d.currie@elsevier.com>
2020-08-14 14:41:17 +0100

    add tests for wildcard and fuzzy search

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+const lunr = require('lunr');
 const testDocs = require('./test-docs');
 const { printResults, createIndex } = require('./lunr-utils');
 const { measure } = require('./utils');
 
-const createFn = createIndex.bind(null, { fields: ['title'] }, testDocs);
+const createFn = createIndex.bind(null,
+  {
+    fields: ['title'],
+    pipeline: [
+      lunr.trimmer,
+      lunr.stopWordFilter,
+      lunr.stemmer,
+    ],
+    searchPipeline: [
+      lunr.stemmer,
+    ]
+  },
+  testDocs
+);
 const index = measure(createFn, `building index for ${testDocs.length} documents`);
 
-const searchFn = index.search.bind(index, 'performance');
-const results = measure(searchFn, 'searching...');
+const performAndMeasureSearch = query => {
+  const searchFn = index.search.bind(index, query);
+  let results = measure(searchFn, `searching for term "${query}"`);
+  printResults(results, testDocs, []);
+}
 
-printResults(results, testDocs, ['title', 'source']);
+console.log('--- SIMPLE SEARCH ---');
+performAndMeasureSearch('neural');
+
+console.log('--- WILDCARD SEARCH ---');
+performAndMeasureSearch('neu*');
+
+console.log('--- FUZZY SEARCH TIGHT ---');
+performAndMeasureSearch('neural~1');
+
+console.log('--- FUZZY SEARCH LOOSE ---');
+performAndMeasureSearch('neural~3');
--- lunr-utils.js
+++ lunr-utils.js
@@ -3,6 +3,18 @@ const lunr = require('lunr');
 function initBuilder(options) {
   const builder = new lunr.Builder();
   builder.ref('id');
+
+  if (options.pipeline) {
+    builder.pipeline.add(
+      ...options.pipeline
+    );
+  }
+
+  if (options.searchPipeline) {
+    builder.searchPipeline.add(
+      ...options.searchPipeline
+    );
+  }
   options.fields.forEach(builder.field.bind(builder));
   return builder;
 }
@@ -14,14 +26,16 @@ function createIndex(options, docs) {
   return builder.build();
 }
 
-function printResults(results, documents, fields) {
-  console.log('results...');
-  const docsMap = documents.reduce((acc, doc) => ({ ...acc, [doc.id]: doc }), {});
-  return results.map(({ ref: id }) =>
-    fields.map(
-      f => console.log(`${f}: ${docsMap[id][f]}`)
-    )
-  );
+function printResults(results, documents, fields = []) {
+  console.log(`------ ${results.length} results ------`);
+  if (fields.length) {
+    const docsMap = documents.reduce((acc, doc) => ({ ...acc, [doc.id]: doc }), {});
+    results.map(({ ref: id }) =>
+      fields.map(
+        field => console.log(`${field}: ${docsMap[id][field]}`)
+      )
+    );
+  }
 }
 
 module.exports = {
