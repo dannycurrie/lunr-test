0fe99c7d44fb511cd37c4c0a5d2c99c7c0b866f9
Danny Currie <d.currie@elsevier.com>
2020-08-18 15:33:10 +0100

    add random test

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { performance } = require('perf_hooks');
 const lunr = require('lunr');
 const testDocs = require('./test-docs');
 const { printResults, createIndex } = require('./lunr-utils');
-const { measure } = require('./utils');
+const { getRandomSearchTerm, measure } = require('./utils');
 
 const createFn = createIndex.bind(null,
   {
@@ -19,20 +20,53 @@ const createFn = createIndex.bind(null,
 );
 const index = measure(createFn, `building index for ${testDocs.length} documents`);
 
-const performAndMeasureSearch = query => {
+const performAndMeasureSearch = (query, fields = []) => {
   const searchFn = index.search.bind(index, query);
   let results = measure(searchFn, `searching for term "${query}"`);
-  printResults(results, testDocs, []);
+  printResults(results, testDocs, fields);
 }
 
-console.log('--- SIMPLE SEARCH ---');
-performAndMeasureSearch('neural');
+const basicTest = () => {
+  console.log('--- SIMPLE SEARCH ---');
+  performAndMeasureSearch('neural');
 
-console.log('--- WILDCARD SEARCH ---');
-performAndMeasureSearch('neu*');
+  console.log('--- WILDCARD SEARCH ---');
+  performAndMeasureSearch('neu*');
 
-console.log('--- FUZZY SEARCH TIGHT ---');
-performAndMeasureSearch('neural~1');
+  console.log('--- FUZZY SEARCH TIGHT (one edit distance) ---');
+  performAndMeasureSearch('neural~1');
 
-console.log('--- FUZZY SEARCH LOOSE ---');
-performAndMeasureSearch('neural~3');
+  console.log('--- FUZZY SEARCH LOOSE (three edit distance) ---');
+  performAndMeasureSearch('neural~3');
+}
+
+const randomTest = () => {
+  const times = Math.trunc(Math.random() * 1000);
+
+  let searchTime = 0;
+
+  for (let i = 0; i <= times; i++) {
+    const before = performance.now();
+    index.search(getRandomSearchTerm());
+    const after = performance.now();
+    searchTime += after - before;
+  }
+
+  console.log('---- SIMPLE RANDOM SEARCH ----');
+  console.log(`${times} searches - average time: ${searchTime / times}ms`)
+}
+
+const args = process.argv.slice(2);
+
+if (args.length) {
+  const arg = args[0];
+
+  switch (arg) {
+    case 'random':
+      randomTest();
+      break;
+    default:
+      console.warn('unrecognised parameter:', arg);
+  }
+} else
+  basicTest();
--- utils.js
+++ utils.js
@@ -1,11 +1,23 @@
+const { performance } = require('perf_hooks');
+const faker = require('faker');
+
+const MAX_TERM_LENGTH = 5;
+
 function measure(fn, label) {
-  console.time(label);
+  const before = performance.now();
   const res = fn();
-  console.timeEnd(label);
+  const after = performance.now();
+  console.log(`${label} - ${after - before}ms`);
   if (res)
     return res;
 }
 
+function getRandomSearchTerm() {
+  const length = faker.random.number(MAX_TERM_LENGTH);
+  return faker.random.words(length);
+}
+
 module.exports = {
-  measure
+  measure,
+  getRandomSearchTerm
 }
